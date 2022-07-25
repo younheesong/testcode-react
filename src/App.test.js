5c64d121c963dc3e42d2a345a8db999d3f1decb7
younheesong <thd6517@naver.com>
2022-07-26 00:04:27 +0900

    test: testcode 작성

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+//test case : counter는 0부터 시작한다.
+
+test("counter start 0", () => {
+  //given : 초기 렌더링
   render(<App />);
-  // render : dom component를 렌더링하는 함수, 인자로 렌더링할 컴포넌트가 들어감.
-  // return은 rl에서 제공하는 쿼리함수와 기타 유틸리티 함수를 담고 있는 객체를 리턴해줌.
-  // 객체를 리턴하기 때문에 원하는 쿼리 함수만 얻어 올 수 있다. (복잡할 경우, screen을 통해 가져오기)
-
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-
-  const linttest = screen.getByRole("button", {
-    name: "linttest",
-  });
-  expect(linttest).toHaveTextContent("linttest");
+  // when: counter에 아무 동작 x
+  const counterElem = screen.getByTestId("counter");
+  // then: counter는 0
+  expect(counterElem).toHaveTextContent(0);
 });
