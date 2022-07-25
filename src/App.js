aa1ecdaee39fcecca3222e6f2d3f8db1b4a778c4
younheesong <thd6517@naver.com>
2022-07-26 00:56:13 +0900

    feat: on/off btn functon

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [isOff, setIsOff] = useState(false);
   function increaseCounter() {
     setCounter((counter) => counter + 1);
   }
@@ -14,13 +15,30 @@ function App() {
     <div className="App">
       <div data-testid="counter">{counter}</div>
       <div>
-        <button data-testid="minus-btn" onClick={() => decreaseCounter()}>
+        <button
+          data-testid="minus-btn"
+          onClick={() => decreaseCounter()}
+          disabled={isOff}
+        >
           -
         </button>
-        <button data-testid="plus-btn" onClick={() => increaseCounter()}>
+        <button
+          data-testid="plus-btn"
+          onClick={() => increaseCounter()}
+          disabled={isOff}
+        >
           +
         </button>
       </div>
+      <button
+        style={{ backgroundColor: "blue" }}
+        data-testid="on-off-btn"
+        onClick={() => {
+          setIsOff((prev) => !prev);
+        }}
+      >
+        on/off
+      </button>
     </div>
   );
 }
