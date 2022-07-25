588bf33fc3c29e6af326c5d3aa315b45ae63253a
younheesong <thd6517@naver.com>
2022-07-26 00:31:56 +0900

    feat: test code based code 작성

--- src/App.js
+++ src/App.js
@@ -4,9 +4,23 @@ import { useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  function increaseCounter() {
+    setCounter((counter) => counter + 1);
+  }
+  function decreaseCounter() {
+    setCounter((counter) => counter - 1);
+  }
   return (
     <div className="App">
       <div data-testid="counter">{counter}</div>
+      <div>
+        <button data-testid="minus-btn" onClick={() => decreaseCounter()}>
+          -
+        </button>
+        <button data-testid="plus-btn" onClick={() => increaseCounter()}>
+          +
+        </button>
+      </div>
     </div>
   );
 }
